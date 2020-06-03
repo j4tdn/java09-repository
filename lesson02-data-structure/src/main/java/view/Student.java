5aae4bafaf66354b12c5dbdcfb48d970a770a1bd
NguyenHong9998 <nguyenhong19999@gmail.com>
2020-06-03 22:54:32 +0700

    lesson02-data-structure 03.06.2020

---
+++ lesson02-data-structure/src/main/java/view/Student.java
@@ -0,0 +1,8 @@
+package view;
+
+public class Student {
+	public static void main(String[] args) {
+		System.out.println("Too late to go back home !!!!");
+		System.out.println("Hello Hong xinh dep");
+	}
+}
