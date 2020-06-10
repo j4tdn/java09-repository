211a6f5833e1533ccd0145792fed4106c110056e
chanh <bosua911999@gmail.com>
2020-06-10 22:47:51 +0700

    lesson02-data-structure 10.06.2018

---
+++ lesson02-data-structure/src/main/java/main/Ex01MainMethod.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Ex01MainMethod {
+	public static void main(String[] args) {
+		System.out.println("wellcome");
+		main();
+		}
+	public void method() {
+		main();
+		}
+	public static void main( ) {
+		System.out.println("second main method");
+	 	}
+
+}
