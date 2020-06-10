5e49d540b4cd794489ecf231cba6077f4c3ff73f
chanh <bosua911999@gmail.com>
2020-06-10 22:47:51 +0700

    lesson02-data-structure 10.06.2018

---
+++ lesson02-data-structure/src/main/java/datatype/Ex01PrimitiveType.java
@@ -0,0 +1,16 @@
+package datatype;
+
+public class Ex01PrimitiveType {
+	
+	private static int f=12;
+	
+	public static void main(String[] args) {
+		int a=10;
+		float b=5.0f;
+		double c=5.3d;
+		char d='d';
+		boolean e=true;
+		
+		System.out.println("a=" + a);
+	}
+}
