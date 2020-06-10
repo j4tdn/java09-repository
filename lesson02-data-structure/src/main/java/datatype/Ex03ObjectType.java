c3370ab0897ff369ab059a3e06896f0882ed33bc
Quyen Phan <Quyen.Phan@mgm-tp.com>
2020-06-10 22:47:36 +0700

    lesson02-data-structure 10.06.2020

---
+++ lesson02-data-structure/src/main/java/datatype/Ex03ObjectType.java
@@ -0,0 +1,28 @@
+package datatype;
+
+public class Ex03ObjectType {
+	public static void main(String[] args) {
+		// Apply with available object types of standard JAVA libs
+		Integer a = 12;
+		Integer aa = new Integer(12);
+		
+		Float b = 5.2f;
+		Double c = 3.3d;
+		Character d = 'd';
+		Boolean e = true;
+		
+		String f = "hello";
+		String g = new String("hello");
+		
+		System.out.println("a: " + a);
+		System.out.println("aa: " + aa);
+		
+		method();
+	}
+	
+	// 1.2
+	@Deprecated
+	private static void method() {
+		
+	}
+}
---
+++ lesson02-data-structure/src/main/java/main/Ex02MultipleClass.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Ex02MultipleClass {
+	public static void main(String[] args) {
+		System.out.println("Ex02MultipleClass");
+	}
+}
+
+class Ex02Fake {
+	public static void main(String[] args) {
+		System.out.println("Ex02Fake ");
+	}
+}
