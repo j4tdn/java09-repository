3ab9471426b452f0698e1793a1bb05294e5c6393
NguyenVantruong <truong1155.gl@gmail.com>
2020-06-14 16:15:19 +0700

    lesson02-data-structure 14.06.2020

---
+++ lesson02-data-structure/src/main/java/enumuration/CirclesPos.java
@@ -0,0 +1,12 @@
+package enumuration;
+
+public enum CirclesPos {
+	INSIDE ("trong Đường tron"),
+	 ONSIDE("tren Đường tron"),
+	 OUTSIDE(" ngoai Đường tron");
+	public String value;
+	
+	CirclesPos(String val){
+		value = val;
+	}
+}
