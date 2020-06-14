407d16a7136fd1772714d2b490d5d1b589247786
ThanhHuyen <heartbolide@gmail.com>
2020-06-14 16:14:58 +0700

    lesson02-data-structure 14.06.2020

---
+++ lesson02-data-structure/src/main/java/random/Ex01.java
@@ -0,0 +1,16 @@
+package random;
+
+import java.util.Random;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Random rd= new Random();
+		int number= rd.nextInt(10);
+		System.out.println(number);
+		
+		String[] students= {"Nam", "Thao", "Hoang"}
+		System.out.println(students[rd.nextInt(students.length)]);
+	}
+
+}
