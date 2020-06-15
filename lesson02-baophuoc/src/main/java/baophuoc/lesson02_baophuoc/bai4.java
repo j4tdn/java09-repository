3531f59d6e43580b787431b85916fa5105f6ef84
OngHuynhBaoPhuoc <onghbaophuoc118@gmail.com>
2020-06-15 20:34:54 +0700

    lesson02-baophuoc-commit code

---
+++ lesson02-baophuoc/src/main/java/baophuoc/lesson02_baophuoc/bai4.java
@@ -0,0 +1,28 @@
+package baophuoc.lesson02_baophuoc;
+
+public class bai4 {
+	public static void main(String[] args) {
+
+		int[] n = { 2, 3, 6, 8, 9, 11, 12, 3, 19, 5, 21, 5 };
+		int max = 0, second = 0;
+
+		for (int i = 0; i <= n.length; i++) {
+
+			for (int j = i + 1; j < n.length; j++) {
+				if (n[i] % 2 != 0 && n[j] % 2 != 0) {
+
+					if (n[i] < n[j] && n[j] > max && n[i] > second) {
+
+						max = n[j];
+						second = n[i];
+					}
+
+				}
+
+			}
+
+		}
+		System.out.println("so le nho thu 2 la :" + second);
+
+	}
+}
---
+++ lesson02-baophuoc/src/main/java/baophuoc/lesson02_baophuoc/bai7.java
@@ -0,0 +1,28 @@
+package baophuoc.lesson02_baophuoc;
+
+import java.util.Random;
+
+public class bai7 {
+	public static void main(String[] args) {
+		int[] a = new int[5];
+		Random rd = new Random();
+		for (int i = 0; i < 5; i++) {
+			boolean trung = false;
+			while (!trung) {
+				trung=true;
+				a[i] = 20 + rd.nextInt(10);
+				for (int j = 0; j < i; j++) {
+					if (a[i] == a[j]) {
+						trung = false;
+						break;
+					}
+				}
+				
+						
+			}
+			System.out.print(a[i] + " ");
+		}
+
+	}
+}
+
