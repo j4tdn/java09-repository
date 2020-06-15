5db95b6f6705a84b332ce8f29cf3e530dffd2df6
chanh <bosua911999@gmail.com>
2020-06-15 18:31:06 +0700

    lesson02-data-structure 14.06.2020

---
+++ lesson02-data-structure/src/main/java/datatype/Int.java
@@ -0,0 +1,14 @@
+package datatype;
+
+public class Int {
+	public int value ;
+	
+	public Int (int val) {
+		value = val;
+	}
+	
+	 @Override
+	public String toString() {
+		return "" + value ;
+	}
+}
