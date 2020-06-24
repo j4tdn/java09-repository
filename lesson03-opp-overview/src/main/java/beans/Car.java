0dec7d525fe45c79f99caa4710c522ed9ed0f7c7
nthuthao <nthuthao0610@gmail.com>
2020-06-24 22:54:04 +0700

    lesson03-opp-overview 24.06.2020

---
+++ lesson03-opp-overview/src/main/java/beans/Car.java
@@ -0,0 +1,86 @@
+package beans;
+
+public class Car {
+	// attributes
+	private String id;
+	private String color;
+	private String model;
+	private Integer capacity; // sức chứa
+
+	// number of people in car at the moment
+	private int currentPassengers;
+
+	// default constructure
+	public Car() {
+
+	}
+
+	// constructor
+	public Car(String pid, String pcolor, String pmodel, Integer pcapacity) {
+		id = pid;
+		color = pcolor;
+		model = pmodel;
+		if (pcapacity == null || pcapacity <= 0) {
+			// bound , unbound, auto-bound
+			throw new RuntimeException("Invalid capacity !");
+		}
+		capacity = pcapacity;
+
+	}
+
+	public void setCapacity(Integer capacity) {
+		if (capacity == null || capacity <= 0) {
+
+			throw new RuntimeException("Invalid capacity !");
+		}
+		System.out.println("come here!");
+		this.capacity = capacity;
+
+	}
+
+	public void setPassengers(int currentAmountOfPeople) {
+		if (currentAmountOfPeople > this.capacity) {
+			System.out.println("customer cannot exceed" + " " + capacity);
+			return;
+		}
+		this.currentPassengers = currentAmountOfPeople;
+
+	}
+
+	@Override
+	public String toString() {
+		return id + " " + color + " " + model + " " + currentPassengers;
+
+	}
+
+	public boolean transfer(Car received, int qty) {
+		int max = received.currentPassengers + qty;
+		int min = currentPassengers - qty;
+
+		if (max > received.capacity || min < 0) {
+			return false;
+		}
+		received.currentPassengers += qty;
+		currentPassengers -= qty;
+		return true;
+
+	}
+
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		c1.id = "123abc";
+		c1.color = "green";
+		c1.model = " benz";
+		//catch error for capacity
+		c1.setCapacity(1);
+		c1.setPassengers(6);
+		Car c2 = new Car("456xyz", "yellow", "lambo", 8);
+		c2.currentPassengers = 3;
+		c1.transfer(c2, 100);
+
+		System.out.println(c1);
+		System.out.println(c2);
+
+	}
+
+}
