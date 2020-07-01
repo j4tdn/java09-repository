40c157d49210f43583d8201c9ef6b2c1ab7c60d7
Meow_Knight <huyviet2582000@gmail.com>
2020-07-01 22:55:00 +0700

    lesson04-oop-features 01.07.2020

---
+++ lesson04-oop-features/src/main/java/polymorphism/Child.java
@@ -0,0 +1,9 @@
+package polymorphism;
+
+public class Child extends Parent{
+	@Override
+	protected void playSport() {
+		System.out.println(getClass().getName() + " play sport...");
+	}
+	
+}
---
+++ lesson04-oop-features/src/main/java/polymorphism/OverrideDemo.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+public class OverrideDemo {
+	public static void main(String[] args) {
+		Parent father = new Parent();
+		father.playSport();
+		
+		System.out.println("================");
+		Parent son = new Child();
+		son.playSport();
+	}
+}
