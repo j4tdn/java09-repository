5c478404ae2ea68899b499ccdd9067ec0bbfb953
NguyenVantruong <truong1155.gl@gmail.com>
2020-07-01 22:55:34 +0700

    lesson04-oop-features 01.07.2020

---
+++ Lesson04-oop-features/src/main/java/outside/Child.java
@@ -0,0 +1,13 @@
+package outside;
+
+import polymorphism.Parent;
+
+public class Child  extends Parent{
+
+	@Override
+	protected void playSprot() {
+		// TODO Auto-generated method stub
+		super.playSprot();
+	}
+	
+}
---
+++ Lesson04-oop-features/src/main/java/polymorphism/Child.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+public class Child extends Parent {
+
+	@Override
+	protected void playSprot() {
+		// TODO Auto-generated method stub
+		System.out.println(getClass().getName()+"playSprot");
+
+	}
+	
+}
