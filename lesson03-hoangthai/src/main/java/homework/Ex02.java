8807bba61c46b7b9c9c19820cd68eaf248e4813f
hoangthai <hoangthai150199@gmail.com>
2020-07-04 22:47:16 +0700

    Lesson03 - HoangThai - Submit code 02

--- lesson03-hoangthai/src/main/java/homework/Ex02.java
+++ lesson03-hoangthai/src/main/java/homework/Ex02.java
@@ -1,8 +1,74 @@
 package homework;
 
+import java.util.Scanner;
+
 public class Ex02 {
-	public static void main(String[] args) {
-		System.out.println("Chưa làm được a ơi");
-		System.out.println("E làm tiếp rồi nạp lại sau ạ");
+	private String chuxe;
+	private String loaixe;
+	private int dungtic;
+	private double trigia;
+	private double thue;
+
+	public String getChuXe() {
+		return chuxe;
+	}
+
+	public void setChuXe(String chuxe) {
+		this.chuxe = chuxe;
+	}
+
+	public String getLoaiXe() {
+		return loaixe;
+	}
+
+	public void setLoaiXe(String loaixe) {
+		this.loaixe = loaixe;
+	}
+
+	public int getDungTic() {
+		return dungtic;
+	}
+
+	public void setDungTic(int dungtic) {
+		this.dungtic = dungtic;
+	}
+
+	public double getTriGia() {
+		return trigia;
+	}
+
+	public void setTriGia(double trigia) {
+		this.trigia = trigia;
+	}
+
+	public double getThue() {
+		return thue;
+	}
+
+	public void setThue(double thue) {
+		this.thue = thue;
+	}
+
+	public void nhapXe() {
+		Scanner sn = new Scanner(System.in);
+		System.out.println("Ten chu xe: ");
+		chuxe = sn.nextLine();
+		System.out.println("Loai xe: ");
+		loaixe = sn.nextLine();
+		System.out.println("Dung tic: ");
+		dungtic = Integer.parseInt(sn.nextLine());
+		System.out.println("Nhap tri gia: ");
+		trigia = Double.parseDouble(sn.nextLine());
+		tinhThue();
+	}
+
+	public void tinhThue() {
+		if (dungtic < 100) {
+			thue = trigia / 100;
+		} else if (dungtic >= 100 && dungtic <= 200) {
+			thue = (trigia / 100) * 3;
+		} else {
+			thue = (trigia / 100) * 5;
+		}
 	}
 }
--- lesson03-hoangthai/src/main/java/homework/Ex03.java
+++ lesson03-hoangthai/src/main/java/homework/Ex03.java
@@ -1,11 +1,40 @@
 package homework;
 
+import java.util.Scanner;
+
 public class Ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Chưa làm được a ơi");
-		System.out.println("E làm tiếp rồi nạp lại sau ạ");
+		Scanner sn = new Scanner(System.in);
+		int chon;
+		Vehicle[] v = new Vehicle[3];
+		v[0] = new Vehicle();
+		v[1] = new Vehicle();
+		v[2] = new Vehicle();
+		int game = 1;
+		do {
+			System.out.println("Chon thao tac: ");
+			chon = Integer.parseInt(sn.nextLine());
+
+			switch (chon) {
+			case 1:
+				for (int i = 0; i < 3; i++) {
+					System.out.println("Nhap xe " + (i + 1));
+					v[i].nhapXe();
+				}
+				break;
+			case 2:
+				System.out.println("Tenchuxe\t\tLoaixe\t\tDungtic\t\tTrigia\t\tThuephainop");
+				for (int i = 0; i < 3; i++) {
+					System.out.println(v[i].getChuXe() + "\t\t" + v[i].getLoaiXe() + "\t\t" + "\t\t" + v[i].getDungTic()
+							+ "\t\t" + v[i].getTriGia() + "\t\t" + v[i].getThue());
+				}
+				break;
+			case 3:
+				game = 0;
+				break;
+			}
+		} while (game == 1);
 	}
 
 }
