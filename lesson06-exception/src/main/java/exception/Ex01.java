97f77ec93600ebc6d7da7b0ad80f28edddf9515d
phuongthao <phuongthao251299.bkdn@gmail.com>
2020-10-04 16:29:54 +0700

    lesson06-exception 04.10.2020

---
+++ lesson06-exception/src/main/java/exception/Ex01.java
@@ -0,0 +1,29 @@
+package exception;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	private static Scanner ip = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Enter number ");
+
+		// \\d: so nguyen [0-9]
+		// +1 : 1 hoac nhieu ky tu
+
+		do {
+			String s = ip.nextLine();
+
+			if (s.matches("\\d+")) {
+				int number = Integer.parseInt(s);
+				System.out.println("Value: " + number);
+				break;
+			}
+
+			else
+				System.out.println("Enter number (\\d+)");
+		}
+		while(true);
+	}
+
+}
