5a472ed1ab2c1e22c8668a85f9d0eed9636b7db8
OngHuynhBaoPhuoc <onghbaophuoc118@gmail.com>
2020-10-19 11:49:26 +0700

    Lesson06-baophuoc-SubmitCode 01

---
+++ lesson06-baophuoc/src/main/java/baophuoc/lesson06_baophuoc/Bai1.java
@@ -0,0 +1,26 @@
+package baophuoc.lesson06_baophuoc;
+
+import java.util.Scanner;
+
+
+public class Bai1 {
+	public static void main(String[] args) {
+		int a, b;
+		float result = 0;
+		Scanner ip = new Scanner(System.in);
+		System.out.println("GIẢI CHƯƠNG TRÌNH BẬC NHẤT");
+		while (true) {
+			try {
+				System.out.print("Nhập a: ");
+				a = Integer.parseInt(ip.nextLine());
+				System.out.print("Nhập b: ");
+				b = Integer.parseInt(ip.nextLine());
+				result = (float) -b / a;
+				break;
+			} catch (NumberFormatException | ArithmeticException e) {
+				System.out.println("vui long nhap lai");
+			}
+		}
+		System.out.println("Kết quả là :" + result);
+	}
+}
---
+++ lesson06-baophuoc/src/main/java/baophuoc/lesson06_baophuoc/Bai2.java
@@ -0,0 +1,45 @@
+package baophuoc.lesson06_baophuoc;
+
+import java.util.Scanner;
+
+public class Bai2 {
+	private static Scanner ip = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String matkhau = null;
+		while (true) {
+			try {
+				matkhau = inputPassword();
+				break;
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+		System.out.println("Mật khẩu của bạn là: " + matkhau);
+
+	}
+
+	private static String inputPassword() throws Exception {
+		System.out.print("nhập nật khẩu: ");
+		String matkhau = ip.nextLine();
+		if (matkhau.length() < 8 || matkhau.length() > 256) {
+			throw new Exception("Password 8->256");
+		}
+		if (!matkhau.matches(".*[a-z]+.*")) {
+			throw new Exception("Phải có ít nhất 1 chữ viết thường");
+		}
+		if (!matkhau.matches(".*[A-Z]+.*")) {
+			throw new Exception("Phải có ít nhất 1 chữ viết hoa");
+		}
+		if (!matkhau.matches(".*\\d.*")) {
+			throw new Exception("Phải có ít nhất 1 chữ số");
+		}
+		if (!matkhau.matches(".*[~!@#$%^&*()_+-=[]\\{}|;':\",.//<>?]]+.*")) {
+			throw new Exception(" Phải có ít nhất 1 ký tự đặc biệt");
+		}
+
+
+		return matkhau;
+	}
+}
