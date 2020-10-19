48ba4bc9ee1645dd1f8fb79be748c33d1b1f31a2
knguyen <nguyen123mitu@gamil.com>
2020-10-19 12:36:34 +0700

    lesson06-khoinguyen-Submit code 01

---
+++ lesson06-exercies/src/test/java/khoinguyen/lesson06_exercises/AppTest.java
@@ -0,0 +1,20 @@
+package khoinguyen.lesson06_exercises;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
