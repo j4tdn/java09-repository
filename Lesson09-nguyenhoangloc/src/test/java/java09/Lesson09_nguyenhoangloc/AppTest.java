016312973a7dddd22c5c7d57ad14b3596c991565
hoanglocqs <nguyenhoangloc1999@gmail.com>
2020-11-11 21:59:43 +0700

    lessson09-hoangloc exam 11-11-2020

---
+++ Lesson09-nguyenhoangloc/src/test/java/java09/Lesson09_nguyenhoangloc/AppTest.java
@@ -0,0 +1,20 @@
+package java09.Lesson09_nguyenhoangloc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
