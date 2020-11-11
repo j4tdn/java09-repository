7967d3ee825614c1f367c172b9da446c9b44ae79
nvtien3042000 <nvtien30042000@gmail.com>
2020-11-11 22:26:18 +0700

    lesson09-nguyenvantien Submit code 2

--- lesson09-nguyenvantien/src/main/java/ex03/Main.java
+++ lesson09-nguyenvantien/src/main/java/ex03/Main.java
@@ -1,8 +1,38 @@
 package ex03;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		String s="xyz";
+		ArrayList<String> strings = permutation(s);
+		for (String string : strings) {
+			System.out.println(string);
+		}
 	}
 
+	
+	public static ArrayList<String> permutation(String s) {
+	    ArrayList<String> res = new ArrayList<String>();
+	    if (s.length() == 1) {
+	        res.add(s);
+	    } else if (s.length() > 1) {
+	        int lastIndex = s.length() - 1;
+	        String last = s.substring(lastIndex);
+	        String rest = s.substring(0, lastIndex);
+	        res = merge(permutation(rest), last);
+	    }
+	    return res;
+	}
+	 
+	public static ArrayList<String> merge(ArrayList<String> list, String c) {
+	    ArrayList<String> res = new ArrayList<String>();
+	    for (String s : list) {
+	        for (int i = 0; i <= s.length(); ++i) {
+	            String ps = new StringBuffer(s).insert(i, c).toString();
+	            res.add(ps);
+	        }
+	    }
+	    return res;
+	}
 }
