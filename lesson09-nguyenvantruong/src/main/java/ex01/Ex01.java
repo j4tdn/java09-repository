f193853309ed58f0128e93a5fe9216d9523f5566
NguyenVantruong <truong1155.gl@gmail.com>
2020-11-11 22:01:47 +0700

    lesson09-exam-11-11-2020

---
+++ lesson09-nguyenvantruong/src/main/java/ex01/Ex01.java
@@ -0,0 +1,27 @@
+package ex01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		Student st1= new Student(01, "nguyenvana", "D");
+		Student st2= new Student(02, "nguyenvanb", "A");
+		Student st3= new Student(03, "nguyenvanc", "F");
+		Student[] students = {st1, st2, st3};
+		
+		
+	}
+	
+//	
+//	private static String getStudent(Student[] students) {
+//		String[] result = new String[students.length];
+//		int index = 0;
+//		for (Student student : students) {
+//			if () {
+//				
+//			}
+//			
+//		}
+//		return null;
+//		
+//	}
+}
+
