67ce93d8fd9d7ae7083b8aa30348dbf8939eabaf
hoangthai <hoangthai150199@gmail.com>
2020-11-11 16:23:06 +0700

    Lesson08-strings 11-11-2020

--- Lesson08-strings/src/main/java/immutable/Ex05.java
+++ Lesson08-strings/src/main/java/immutable/Ex05.java
@@ -1,16 +1,26 @@
 package immutable;
 
 public class Ex05 {
-public static void main(String[] args) {
-	String input = "1, 2, 2, 3";
-}
-private String getUniqueNbers(String intput) {
-	String result = " ";
+	public static void main(String[] args) {
+		String input = "1, 2, 2, 3";
+		System.out.println(getUniqueNbers(input));
+	}
+
+private static String getUniqueNbers(String input) {
+	String result = "";
+	String[] numbers = input.trim().split(",");
 	boolean[] flags = new boolean[numbers.length];
-	for(int out = 0, out < numbers.length; out++) {
+	for(int out = 0; out < numbers.length; out++) {
 		if(!flags[out]) {
-			for(int in = out + 1; in < numbers.length, in++);
+			for(int in = out + 1; in < numbers.length; in++);
+					if (numbers[out].equals(numbers[in])) {
+						flags[out] = true;
+						flags[in] = true;
+					}
 		}
+	}
+	if(!flags[out]) {
+		result += numbers[out]+",";
 	}
 	return result;
 }
--- Lesson08-strings/src/main/java/immutable/Ex06.java
+++ Lesson08-strings/src/main/java/immutable/Ex06.java
@@ -1,13 +1,15 @@
 package immutable;
 
+import java.util.Arrays;
+
 public class Ex06 {
 	public static void main(String[] args) {
 		String students = " LeNa, LeTeo, HoangNa, VanTeo, LeTa";
-		System.out.println(String.join(" - ", result));
+		System.out.println(String.join(" , ", getStudents(students, "Le")));
 	}
-	private static String[] getStudents(String students, String prefix);{
+	private static String[] getStudents(String students, String prefix) {
 		String[] studentArray = students.split(" , ");
-		String[] result = new String[studenArray.length()];
+		String[] result = new String[studentArray.length];
 		int index = 0;
 		for(String student: studentArray) {
 			if(student.startsWith(prefix)) {
