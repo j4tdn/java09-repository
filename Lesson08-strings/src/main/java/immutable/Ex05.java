7249b96da5a94c54fe1cfbec98586dd7e33b3afe
hoangthai <hoangthai150199@gmail.com>
2020-11-11 22:09:21 +0700

    Lesson08-strings 11-11-2020

--- Lesson08-strings/src/main/java/immutable/Ex05.java
+++ Lesson08-strings/src/main/java/immutable/Ex05.java
@@ -12,16 +12,20 @@ private static String getUniqueNbers(String input) {
 	boolean[] flags = new boolean[numbers.length];
 	for(int out = 0; out < numbers.length; out++) {
 		if(!flags[out]) {
-			for(int in = out + 1; in < numbers.length; in++);
+			for(int in = out + 1; in < numbers.length; in++) {
 					if (numbers[out].equals(numbers[in])) {
 						flags[out] = true;
 						flags[in] = true;
 					}
 		}
+		if(!flags[out]) {
+			result += numbers[out]+",";
 	}
-	if(!flags[out]) {
-		result += numbers[out]+",";
+	
 	}
+	
+}
 	return result;
 }
+
 }
