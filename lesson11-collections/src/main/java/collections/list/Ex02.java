2430605ea4f91b09f1b338534a108ccfd3cc6fd3
chanh <bosua911999@gmail.com>
2020-11-22 16:30:02 +0700

    lesson11-collections-22-11-2020

---
+++ lesson11-collections/src/main/java/collections/list/Ex02.java
@@ -0,0 +1,19 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> digits = new ArrayList<>();
+		
+		digits.add(1);
+		digits.add(2);
+		digits.add(3);
+		
+		System.out.println(digits);
+	}
+
+}
