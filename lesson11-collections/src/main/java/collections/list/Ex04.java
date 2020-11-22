e1ad1e87df2dacd2f2992796da78304b97e92f2f
ThanhHuyen <heartbolide@gmail.com>
2020-11-22 16:29:20 +0700

    lesson11-collections 22.11.2020

---
+++ lesson11-collections/src/main/java/collections/list/Ex04.java
@@ -0,0 +1,23 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static collections.utils.ListUtils.*;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		List<String> students = Arrays.asList(
+				"Nam", "Hoang", "Thuong", "Nguyen"
+				);
+		
+		Collections.reverse(students);
+		Collections.shuffle(students);
+		printf(students);
+		
+		System.out.println("contains: " + students.contains("Nam"));
+	}
+}
---
+++ lesson11-collections/src/main/java/collections/utils/ListUtils.java
@@ -0,0 +1,15 @@
+package collections.utils;
+
+import java.util.List;
+
+public class ListUtils {
+	private ListUtils() {
+		
+	}
+	
+	public static void printf(List<String> elements) {
+		for (String element:elements) {
+			System.out.println(element);
+		}
+	}
+}
