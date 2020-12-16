4fcbef2bc940e65ebc561ed4232972e0832e325f
chanh <bosua911999@gmail.com>
2020-12-16 22:37:31 +0700

    lesson12-streamapi-16/12/2020

---
+++ lesson12-streamapi/src/main/java/view/UsingLocalVariable.java
@@ -0,0 +1,30 @@
+package view;
+
+public class UsingLocalVariable {
+	
+	private static String globalVariable = "global";
+	public static void main(String[] args) {
+		
+	}
+	
+	private static void method() {
+		String localVariable= "local";
+		Runnable runnable = new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				String innerVariable= "inner";
+				
+				System.out.println(localVariable);
+				System.out.println(localVariable.equals(innerVariable));
+				globalVariable = "global-local";
+				System.out.println(globalVariable);
+			}
+			
+			
+		};
+		runnable.run();
+	}
+
+}
