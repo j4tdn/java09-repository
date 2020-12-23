993da99b5ba31a860c506d186e117cb404e5d2be
hoanglocqs <nguyenhoangloc1999@gmail.com>
2020-12-23 22:40:09 +0700

    streamAPI 23-12-2020

---
+++ lesson12-streamsAPI/src/main/java/stream/Ex02FilteringAndSlising.java
@@ -0,0 +1,34 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import com.sun.tools.javac.util.List;
+
+import bean.Apple;
+import service.AppleService;
+import service.AppleServiceImpl;
+
+public class Ex02FilteringAndSlising {
+	private static AppleService service;
+
+	static {
+		service = new AppleServiceImpl();
+	}
+	public static void main(String[] args) {
+		String[] data = Pattern.compile("[,\\s]+").splitAsStream("a,b,c,d")
+				
+				.distinct()
+				.skip(1)
+				.limit(3)
+				.toArray(String[]::new);				;
+//		Arrays.stream(data).forEach(item->SystemAppleprintln(item+" "));
+		
+		
+		java.util.List<Apple> inventory=service.getAll();
+		inventory.
+		stream()
+		.distinct()
+		.forEach(System.out::println);
+	}
+}
