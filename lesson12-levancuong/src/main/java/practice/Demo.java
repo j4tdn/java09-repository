a7d21be6dc88e3c21386e0de67cadd0af282c0a6
levancuong <levancuong150599@gmail>
2021-01-02 22:59:31 +0700

    lesson12-levancuong submit code 01

---
+++ lesson12-levancuong/src/main/java/practice/Demo.java
@@ -0,0 +1,40 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Demo {
+public static void main(String[] args) {
+	
+	Trader raoul=new Trader("Raoul", "Cambridge");
+	Trader mario=new Trader("Mario", "Milan");
+	Trader alan=new Trader("Alan", "Cambridge");
+	Trader brian=new Trader("Brian", "Cambridge");
+	
+	
+	List<Trader> traders=new ArrayList<Trader>();
+			traders.add(raoul);
+			traders.add(mario);
+			traders.add(alan);
+			traders.add(brian);
+			
+			
+
+	
+	List<Transaction> transactions=Arrays.asList(
+			new Transaction(brian,2011,300),
+			new Transaction(raoul,2012,1000),
+			new Transaction(raoul,2011,400),
+			new Transaction(mario,2012,710),
+			new Transaction(mario,2012,700),
+			new Transaction(alan,2012,950)
+			
+			);
+	
+	
+	
+}
+}
