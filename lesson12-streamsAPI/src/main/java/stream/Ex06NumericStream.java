190792d61601e6b9670e55c944c5c42279bf36e5
hoanglocqs <nguyenhoangloc1999@gmail.com>
2021-01-06 22:33:41 +0700

    lesson12-lesson13 06-01-2020

---
+++ lesson12-streamsAPI/src/main/java/stream/Ex06NumericStream.java
@@ -0,0 +1,33 @@
+package stream;
+
+import java.util.List;
+import java.util.OptionalDouble;
+
+import bean.Apple;
+import service.AppleService;
+import service.AppleServiceImpl;
+
+public class Ex06NumericStream {
+	
+	private static AppleService service;
+	
+	static {
+		service=new AppleServiceImpl();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		//find apple whit hig
+		List<Apple> inventory =service.getAll();
+		OptionalDouble optional = inventory.stream()
+		.mapToDouble(Apple::getWeight)
+		.max();
+		
+		
+		System.out.println(optional.orElse(Double.MIN_VALUE));
+		
+		
+		
+	}
+}
---
+++ lesson13-FileApi/src/main/java/definition/Ex01.java
@@ -0,0 +1,45 @@
+package definition;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.management.RuntimeErrorException;
+
+public class Ex01 {
+	// ex01: create file data.txt
+	public static void main(String[] args) throws Exception {
+		File file = createFile("data.txt");
+
+		// info file
+		System.out.println("name   " + file.getName());
+		System.out.println("path   " + file.getPath());
+		System.out.println("absolutepath   " + file.getAbsolutePath());
+		System.out.println("canonical    " + file.getCanonicalPath());
+		
+		
+		Path path=Paths.get("info.txt");
+		 Files.createFile(path);
+		 System.out.println("name   "+path.getName(0));
+	}
+
+	private static File createFile(String path) {
+
+		boolean isSuccess = false;
+		File file = new File(path);
+		if (!file.exists()) {
+			try {
+				isSuccess = file.createNewFile();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (!isSuccess) {
+			System.out.println(file.getName() + "is existed");
+		}
+		return file;
+	}
+}
