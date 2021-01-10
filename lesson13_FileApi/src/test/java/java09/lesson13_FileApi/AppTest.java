bae4a144b601cc5fd2e90c1a171d51f9e46d32bd
hoangthai <hoangthai150199@gmail.com>
2021-01-10 12:58:47 +0700

    lesson13_FileApi 06-01-2021

---
+++ lesson13_FileApi/src/test/java/java09/lesson13_FileApi/AppTest.java
@@ -0,0 +1,20 @@
+package java09.lesson13_FileApi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
