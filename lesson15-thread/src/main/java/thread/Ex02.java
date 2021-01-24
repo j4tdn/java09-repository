7a0d056ee467e17444c72de3c43f459001193fd7
OngHuynhBaoPhuoc <onghbaophuoc118@gmail.com>
2021-01-24 16:29:41 +0700

    Lesson15-thread-24/01/2021

---
+++ lesson15-thread/src/main/java/thread/Ex02.java
@@ -0,0 +1,19 @@
+package thread;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println("Thread main start");
+		Thread t0 = new Thread(new Runnable() {
+			
+			@Override
+			//task
+			public void run() {
+				System.out.println(Thread.currentThread().getName()+ " is running ....");
+			}
+		}, "t0");
+		t0.start();
+		
+		
+		System.out.println("Thread main end");
+	}
+}
---
+++ lesson15-thread/src/main/java/thread/Ex03.java
@@ -0,0 +1,37 @@
+package thread;
+
+import java.awt.peer.SystemTrayPeer;
+import java.util.concurrent.TimeUnit;
+
+public class Ex03 {
+	private static long start=0;
+	public static void main(String[] args) {
+		
+		start=System.currentTimeMillis();
+		System.out.println("Thread main start");
+		
+		Thread t0 = new Thread(new Task(),"t0");
+		Thread t1 = new Thread(new Task(),"t1");
+		t0.start();
+		t1.start();
+		
+		System.out.println("Thread main end");
+		System.out.println("Thread main:" + (System.currentTimeMillis()-start));
+	}
+	
+	private static class Task implements Runnable {
+		public void run() {
+			try {
+				TimeUnit.SECONDS.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+ " is running...");		
+			System.out.println(Thread.currentThread().getName()+ " ");
+		}
+	}
+
+	}
+
+
