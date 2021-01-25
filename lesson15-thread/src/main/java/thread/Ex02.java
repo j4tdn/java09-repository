054319eaabd76a9ee92062af814a4523de766153
ThanhHuyen <heartbolide@gmail.com>
2021-01-25 22:44:55 +0700

    lesson15-thread 25.1.2021

---
+++ lesson15-thread/src/main/java/thread/Ex02.java
@@ -0,0 +1,20 @@
+package thread;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println("Thread main start");
+		
+		Thread t0 = new Thread(new Runnable() {
+			
+			//task
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName() + " is running ...");
+			}
+		}, "t0");
+		t0.start();
+		
+		System.out.println("Thread main end");
+		
+	}
+}
---
+++ lesson15-thread/src/main/java/thread/pool/Ex03.java
@@ -0,0 +1,68 @@
+package thread.pool;
+
+import java.lang.management.ThreadMXBean;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import manager.ThreadManager;
+import manager.ThreadManagerImpl;
+
+public class Ex03 {
+	
+	private static final int THREAD_POOL_SIZE;
+	private static ExecutorService executor;
+	private static ThreadManager threadManager;
+	
+	static {
+		THREAD_POOL_SIZE = 4;
+		executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+		threadManager = new ThreadManagerImpl();
+	}
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		//Future, FutureTask, Callable
+		//3 queries
+		List<Integer> intList = Arrays.asList(1,2,3,4,5);
+		List<String> stringList = Arrays.asList("A", "B", "C", "D", "E");
+		List<Double> doubleList = Arrays.asList(10d, 11d, 12d, 13d, 14d, 15d);
+		
+		FutureTask<List<Integer>> integerTask = 
+				new FutureTask<List<Integer>>(() -> {
+					TimeUnit.SECONDS.sleep(3);
+					return Arrays.asList(1,2,3,4,5);
+				});
+		FutureTask<List<String>> stringTask = 
+				new FutureTask<List<String>>(() -> {
+					TimeUnit.SECONDS.sleep(3);
+					return Arrays.asList("A", "B", "C", "D", "E");
+				});
+		FutureTask<List<Double>> doubleTask = 
+				new FutureTask<List<Double>>(() -> {
+					TimeUnit.SECONDS.sleep(3);
+					return Arrays.asList(10d, 11d, 12d, 13d, 14d, 15d);
+				});
+	
+		runTasks(Arrays.asList(integerTask, stringTask, doubleTask));
+		
+		List<Integer> integerResult = integerTask.get();
+		List<String> stringResult = stringTask.get();
+		List<Double> doubleResult = doubleTask.get();
+		
+		System.out.println("integer size: " + integerResult.size());
+		System.out.println("string size: " + stringResult.size());
+		System.out.println("double size: " + doubleResult.size());
+		
+	}
+	
+	private static void runTasks(List<FutureTask<?>> tasks) {
+		tasks.forEach(task -> threadManager.safelyScheduleTask(executor, task));
+	}
+}
