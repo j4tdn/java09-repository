e147f037e4bf03f5f005678c5076d225d7a849ed
hoangthai <hoangthai150199@gmail.com>
2021-01-25 11:56:46 +0700

    lesson14-hoangthai 25-01-2021

---
+++ lesson14-hoangthai/src/main/java/ex01/Ex01.java
@@ -0,0 +1,13 @@
+package ex01;
+
+public class Ex01 
+{
+    public static void main( String[] args )
+    {
+    	int[] input = {1, 2, 3, 4, 5, 2, 3};
+    	
+        System.out.println("=====");
+    }
+    
+    
+}
