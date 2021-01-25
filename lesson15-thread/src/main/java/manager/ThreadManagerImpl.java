35467381c5e919acbeda91385b099070530c795a
thaibinh <nguyenthaibinh16t2bkdn@gmail.com>
2021-01-25 22:45:29 +0700

    lesson15-thread 25-01-2020

---
+++ lesson15-thread/src/main/java/manager/ThreadManagerImpl.java
@@ -0,0 +1,25 @@
+package manager;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ThreadManagerImpl implements ThreadManager{
+
+	@Override
+	public Future<?> safelyScheduleTask(ExecutorService executor, Runnable runnable) {
+		
+		return runTask(executor, runnable);
+	}
+	
+	private Future<?> runTask(ExecutorService excutor, Runnable runnable) {
+		return excutor.submit(new Runnable() {
+			
+			@Override
+			public void run() {
+				runnable.run();
+				
+			}
+		});
+	}
+}
