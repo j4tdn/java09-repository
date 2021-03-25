0f0e2dbdcfa67036bb6f1be12134f4f58f69525b
huyenpoo <heartbolide@gmail.com>
2021-03-25 22:07:58 +0700

    lesson17-database-connection 25.03.2021

---
+++ lesson17-database-connection/src/main/java/dao/ItemGroupDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import connection.DBConnection;
+import persistence.ItemGroup;
+
+public class ItemGroupDao {
+	public List<ItemGroup> getAll(){
+		List<ItemGroup> result = new ArrayList<>();
+		
+		Connection conn = DBConnection.getConnection();
+		String sql = "SELECT * FROM LoaiHang";
+		
+		try {
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next()) {
+				ItemGroup ig = new ItemGroup(rs.getInt("MaLoai"), rs.getString("TenLoai"));
+				result.add(ig);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+}
