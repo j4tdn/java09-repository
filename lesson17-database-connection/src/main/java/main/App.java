ff9e4672b8389e8e26b83140f28fa08bad0da072
huyenpoo <heartbolide@gmail.com>
2021-04-19 22:50:55 +0700

    lesson18-hibernate 19.4.2021

--- lesson17-database-connection/src/main/java/main/App.java
+++ lesson17-database-connection/src/main/java/main/App.java
@@ -2,12 +2,16 @@ package main;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import dao.EmployeeDao;
 import dao.ItemDao;
 import dao.ItemGroupDao;
 import dao.OrderDao;
+import persistence.Employee;
 import persistence.Item;
 import persistence.ItemGroup;
 import persistence.ItemGroupDto;
@@ -16,11 +20,13 @@ public class App {
 	private static ItemGroupDao itemGroupDao;
 	private static ItemDao itemDao;
 	private static OrderDao orderDao;
+	private static EmployeeDao employeeDao;
 	
 	static {
 		itemGroupDao = new ItemGroupDao();
 		itemDao = new ItemDao();
 		orderDao = new OrderDao();
+		employeeDao = new EmployeeDao();
 	}
 	
 	public static void main(String[] args) {
@@ -42,9 +48,26 @@ public class App {
 		List<ItemGroupDto> igrDetails = itemGroupDao.getItemDetails();
 		show(igrDetails);
 		
-		System.out.println("==========");
+		System.out.println("==============");
 		String pass = "12345";
 		System.out.println("MD5: " + DigestUtils.md5Hex(pass));
+		
+		System.out.println("==============");
+		
+		while (true) {
+			Scanner ip = new Scanner(System.in);
+			System.out.println("Username: " );
+			String username = ip.nextLine();
+			System.out.println("Password: ");
+			String password = ip.nextLine();
+			Optional<Employee> emp = employeeDao.login(username, password);
+			if (emp.isPresent()) {
+				System.out.println("UserInfo: " + emp.get());
+				break;
+			} else {
+				System.out.println("Wrong user/pass");
+			}
+		}
 	}
 	
 	private static <E> void show(List<E> es) {
