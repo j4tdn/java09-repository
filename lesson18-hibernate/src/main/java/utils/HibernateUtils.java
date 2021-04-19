1b7a56742fab2ad1d7ab805f6beacbccfc3b223d
levancuong <levancuong150599@gmail.com>
2021-04-19 22:51:29 +0700

    lesson 18-hibernate 19.04.2021

---
+++ lesson18-hibernate/src/main/java/utils/HibernateUtils.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+	private static SessionFactory sessionFactory = null;
+
+	private HibernateUtils() {
+
+	}
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration configuration = new Configuration();
+			sessionFactory = configuration.configure("hibernate.cfg.xml").buildSessionFactory();
+
+		}
+		return sessionFactory;
+	}
+
+	public static void main(String[] args) {
+		SessionFactory sf = getSessionFactory();
+		System.out.println("sf:" + sf.openSession());
+	}
+}
